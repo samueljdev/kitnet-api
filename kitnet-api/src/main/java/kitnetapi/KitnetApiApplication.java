2ac465869e9824490cb24ed3b6287e837bc3c72c
Your Name <you@example.com>
2023-01-25 15:57:52 -0400

    criando projeto

---
+++ kitnet-api/src/main/java/kitnetapi/KitnetApiApplication.java
@@ -0,0 +1,13 @@
+package kitnetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitnetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitnetApiApplication.class, args);
+	}
+
+}
---
+++ kitnet-api/src/test/java/kitnetapi/KitnetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kitnetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitnetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
